bb91a982618d1576946bcd8677983ac6d4aabf5a
remvst <remvst@gmail.com>
2022-07-14 01:33:28 -0400

    Build

--- lib/rectangle.d.ts
+++ lib/rectangle.d.ts
@@ -1,13 +1,15 @@
 import Segment from './segment';
 export default class Rectangle {
-    private _sides;
-    private _dirtySides;
+    private readonly _sides;
+    private dirtySides;
     x: number;
     y: number;
     width: number;
     height: number;
     constructor(x: number, y: number, width: number, height: number);
     get sides(): Segment[];
+    get minX(): number;
+    get minY(): number;
     get maxX(): number;
     get maxY(): number;
     centerAround(x: number, y: number, width?: number, height?: number): void;
